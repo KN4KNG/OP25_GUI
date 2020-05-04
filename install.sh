c5c747bc614ab85e706901b286bf783d632a105c
Alex Bowman <KN4KNG@outlook.com>
2020-05-04 19:45:24 +0100

    Create install.sh
    Create Install script for testing

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~
+sudo cp ~/OP25_GUI/NC_Counties_GUI/NC_Viper/ ~/op25/op25/gr-op25_repeater/apps/
+sudo cp ~/OP25_GUI/NC_Counties_GUI/*.py ~/Desktop/
+sudo chmod +x ~/Desktop/North_Carolina_Viper_GUI_Non_USB_Speaker_Default_Audio.py
+sudo chmod +x ~/Desktop/North_Carolina_Viper_GUI.py
