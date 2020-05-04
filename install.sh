b04fac29e7dad4e98eb59ba67218a15128891d63
Alex Bowman <KN4KNG@outlook.com>
2020-05-04 15:21:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 cd /home/pi
 sudo cp -r /home/pi/OP25_GUI/NC_Counties_GUI/NC_Viper/ /home/pi/op25/op25/gr-op25_repeater/apps/
 sudo cp /home/pi/OP25_GUI/NC_Counties_GUI/*.py /home/pi/Desktop/
-sudo chmod +x /home/pi/Desktop/North_Carolina_Viper_GUI_Non_USB_Speaker_Default_Audio.py
-sudo chmod +x /home/pi/Desktop/North_Carolina_Viper_GUI.py
+sudo chmod 755 /home/pi/Desktop/North_Carolina_Viper_GUI_Non_USB_Speaker_Default_Audio.py
+sudo chmod 755 /home/pi/Desktop/North_Carolina_Viper_GUI.py
